2b771ae141e0e134ce0af5a191ea2432f7a9448c
marius.scurtescu <marius.scurtescu@32d0d7b0-0701-11de-ba4d-252e5d5a8945>
2009-03-02 09:10:53 +0000

    Initial checkin, skeleton

---
+++ src/com/googlecode/build_commander/CommanderListener.java
@@ -0,0 +1,52 @@
+package com.googlecode.build_commander;
+
+import org.apache.tools.ant.SubBuildListener;
+import org.apache.tools.ant.BuildEvent;
+
+public class CommanderListener implements SubBuildListener
+{
+    public void subBuildStarted(BuildEvent event)
+    {
+        //To change body of implemented methods use File | Settings | File Templates.
+    }
+
+    public void subBuildFinished(BuildEvent event)
+    {
+        //To change body of implemented methods use File | Settings | File Templates.
+    }
+
+    public void buildStarted(BuildEvent event)
+    {
+        //To change body of implemented methods use File | Settings | File Templates.
+    }
+
+    public void buildFinished(BuildEvent event)
+    {
+        //To change body of implemented methods use File | Settings | File Templates.
+    }
+
+    public void targetStarted(BuildEvent event)
+    {
+        //To change body of implemented methods use File | Settings | File Templates.
+    }
+
+    public void targetFinished(BuildEvent event)
+    {
+        //To change body of implemented methods use File | Settings | File Templates.
+    }
+
+    public void taskStarted(BuildEvent event)
+    {
+        //To change body of implemented methods use File | Settings | File Templates.
+    }
+
+    public void taskFinished(BuildEvent event)
+    {
+        //To change body of implemented methods use File | Settings | File Templates.
+    }
+
+    public void messageLogged(BuildEvent event)
+    {
+        //To change body of implemented methods use File | Settings | File Templates.
+    }
+}
