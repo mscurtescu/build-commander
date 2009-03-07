dbe32bb81c3395841fd2e48f8318f166789ee1bb
marius.scurtescu <marius.scurtescu@32d0d7b0-0701-11de-ba4d-252e5d5a8945>
2009-03-07 08:00:13 +0000

    addedd 'success' property and logging for conditions

--- trunk/src/com/googlecode/build_commander/Condition.java
+++ trunk/src/com/googlecode/build_commander/Condition.java
@@ -2,22 +2,31 @@ package com.googlecode.build_commander;
 
 import org.apache.tools.ant.PropertyHelper;
 
+import java.util.logging.Logger;
+
 public class Condition
 {
+    private Config _config;
+    private Logger _logger;
     private String _property;
     private String _value;
     private boolean _negate;
 
-    public static Condition create(String expression, String value, boolean negate)
+    public static Condition create(Config config, String expression, String value, boolean negate)
     {
-        return new Condition(expression, value, negate);
+        return new Condition(config, expression, value, negate);
     }
 
-    private Condition(String property, String value, boolean negate)
+    private Condition(Config config, String property, String value, boolean negate)
     {
+        _config = config;
+        _logger = config.getLogger();
+
         _property = property;
         _value = value;
         _negate = negate;
+
+        _logger.finer("Condition created: " + property + " = " + value);
     }
 
     public boolean evaluate(PropertyHelper propertyHelper)
@@ -25,6 +34,8 @@ public class Condition
         String left = (String) propertyHelper.getProperty(null, _property);
         String right = propertyHelper.replaceProperties(null, _value, null);
 
+        _logger.finer("Condition evaluated: " + left + " = " + right);
+        
         boolean eq = left.equals(right);
 
         if (_negate)
